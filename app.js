f9d71e88c6493254fbc8ddcf8e42b7e3f9c87613
nok-chan <HrithikSarkar45@protonmail.com>
2022-06-27 19:55:39 +0530

    added a clear button using ctx.clearRect(0, 0, canvas.width, canvas.height) and called the function whenever a button is clicked. Called both drawCircle and drawLine fn. to draw thick lines and fill color. The app somewhat works

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ const increaseBtn = document.getElementById("increase");
 const decreaseBtn = document.getElementById("decrease");
 const radiusValue = document.querySelector(".radius-value");
 const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
 const ctx = canvas.getContext("2d");
 
 let radius = 15;
 let isPressed = false;
-let color = "grey";
+let color = "black";
 let x = undefined;
 let y = undefined;
 
-canvas.addEventListener("mousedown", (e)=>{
-   isPressed = true;
+canvas.addEventListener("mousedown", (e) => {
+    isPressed = true;
 
-   x = e.offsetX;
-   y = e.offsetY;
+    x = e.offsetX;
+    y = e.offsetY;
 });
 
-canvas.addEventListener("mouseup", () =>{
+canvas.addEventListener("mouseup", () => {
     isPressed = false;
 
     x = undefined;
@@ -26,16 +27,18 @@ canvas.addEventListener("mouseup", () =>{
 });
 
 canvas.addEventListener("mousemove", (e) => {
-    if(isPressed){
-    const x2 = e.offsetX;
-    const y2 = e.offsetY;
-    drawLine(x, y, x2, y2);
-    x = x2;
-    y = y2;
+    if (isPressed) {
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
+
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+        x = x2;
+        y = y2;
     }
 });
 
-function drawCircle(x, y){
+function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI)
     ctx.fillStyle = color;
@@ -47,45 +50,34 @@ function drawLine(x1, y1, x2, y2) {
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.strokeStyle = color;
-    ctx.linewidth = radius * 2;
+    ctx.lineWidth = radius * 2;
     ctx.stroke();
 }
 
 increaseBtn.addEventListener("click", () => {
     radius += 5;
 
-    if(radius > 30){
+    if (radius > 30) {
         radius = 30;
     }
     updateSizeOnScreen();
 });
 decreaseBtn.addEventListener("click", () => {
     radius -= 5;
-    if(radius < 5){
+    if (radius < 5) {
         radius = 5;
     }
     updateSizeOnScreen();
 });
 
-colorEl.addEventListener("change", (e) =>{
+colorEl.addEventListener("change", (e) => {
     color = e.target.value;
 });
 
-function updateSizeOnScreen(){
+function updateSizeOnScreen() {
     radiusValue.textContent = radius;
 }
 
-
-// function draw will be called every frame
-// function draw(){
-
-//     // clear canvas
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//     drawCircle(x++, y);
-
-//     requestAnimationFrame(draw);
-
-// }
-
-// draw();
+clearEl.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
