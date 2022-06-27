5ae984856ecbd129ecc75628a7406ad8ff15d76e
nok-chan <HrithikSarkar45@protonmail.com>
2022-06-27 14:53:28 +0530

    adding canvas draw line

--- app.js
+++ app.js
@@ -8,20 +8,28 @@ const ctx = canvas.getContext("2d");
 let radius = 15;
 let isPressed = false;
 let color = "grey";
+let x = undefined;
+let y = undefined;
 
-canvas.addEventListener("mousedown", ()=>{
+canvas.addEventListener("mousedown", (e)=>{
    isPressed = true;
+
+   x = e.offsetX;
+   y = e.offsetY;
 });
 
 canvas.addEventListener("mouseup", () =>{
     isPressed = false;
+
+    x = undefined;
+    y = undefined;
 });
 
 canvas.addEventListener("mousemove", (e) => {
     if(isPressed){
-    const x = e.offsetX;
-    const y = e.offsetY;
-    drawCircle(x, y);
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+    drawLine(x, y, x2, y2);
     }
 });
 
@@ -30,6 +38,15 @@ function drawCircle(x, y){
     ctx.arc(x, y, radius, 0, 2 * Math.PI)
     ctx.fillStyle = color;
     ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.linewidth = radius * 2;
+    ctx.stroke();
 }
 
 increaseBtn.addEventListener("click", () => {
